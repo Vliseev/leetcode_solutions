d11c53e6a9113952aaf15fdc25e23be9492d175a
vlad <Vliseev.92@mail.ru>
2023-02-12 10:28:43 +0300

    Create solution1.rs

---
+++ 203. Remove Linked List Elements/solution1.rs
@@ -0,0 +1,66 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn remove_elements(mut head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+        let mut dummy = None;
+        let mut tail = &mut dummy;
+        while let Some(mut node) = head.take() {
+            head = node.next.take();
+            if node.val != val {
+                *tail = Some(node);
+                tail = &mut tail.as_mut().unwrap().next;
+            }
+        }
+        dummy
+    }
+}
+
+/*
+ *
+#[macro_export]
+macro_rules! linkedlist {
+    () => {
+        None
+    };
+    ($($e:expr), *) => {
+        {
+            let mut head = Box::new($crate::ListNode::new(0));
+            let mut ref_head = &mut head;
+
+            $(
+            ref_head.next = Some(Box::new($crate::ListNode::new($e)));
+            ref_head = ref_head.next.as_mut().unwrap();
+            )*
+
+            let _ = ref_head;
+            head.next
+        }
+    };
+}
+
+let mut list = linkedlist![1, 2, 3, 4, 5];
+let mut res = Solution::remove_elements(list, 5);
+println!("{res:?}");
+
+list = linkedlist![1,2,6,3,4,5,6];
+res = Solution::remove_elements(list, 6);
+println!("{res:?}");
+
+list = linkedlist![7, 7, 7];
+res = Solution::remove_elements(list, 7);
+println!("{res:?}");
+ */
